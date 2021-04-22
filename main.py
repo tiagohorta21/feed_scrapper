bc52ac15f1d6c3d77a6aebedee6f0cc5da3a4fb6
Tiago Horta <tiago_horta95@hotmail.com>
2021-04-22 14:53:54 +0100

    main: Add list add and remove functions

--- main.py
+++ main.py
@@ -2,16 +2,34 @@
 import sqlite3
 import feedparser
 
-# TODO: Based on the parameter get the feed but do not save it in the database
+def listFeeds():
+    # Used RSS CNN News - http://rss.cnn.com/rss/edition.rss
+    response = feedparser.parse("http://rss.cnn.com/rss/edition.rss")
+    print("\nCurrent feeds:")
+    for count, entry in enumerate(response.entries):
+      print(str(count + 1) + " - " + entry.title)
 
-# Used RSS CNN News - http://rss.cnn.com/rss/edition.rss
-response = feedparser.parse("http://rss.cnn.com/rss/edition.rss")
-for entry in response.entries:
+def addFeed():
+    # Used RSS CNN News - http://rss.cnn.com/rss/edition.rss
+    response = feedparser.parse("http://rss.cnn.com/rss/edition.rss")
+    for entry in response.entries:
+        # Open database connection
+        connection = sqlite3.connect("tp1.db")
+        cursor = connection.cursor()
+        # Insert feed and parameters into database
+        cursor.execute("INSERT INTO Feeds VALUES (?,?,?)", (None, entry.link, entry.title))
+        # Save (commit) the changes
+        connection.commit()
+        # We can also close the connection if we are done with it.
+        # Just be sure any changes have been committed or they will be lost.
+        connection.close()
+
+def removeFeeds():
     # Open database connection
     connection = sqlite3.connect("tp1.db")
     cursor = connection.cursor()
-    # Insert feed and parameters into database
-    cursor.execute("INSERT INTO Feeds VALUES (?,?,?)", (None, entry.link, entry.title))
+    # Delete all feeds
+    cursor.execute("DELETE FROM Feeds")
     # Save (commit) the changes
     connection.commit()
     # We can also close the connection if we are done with it.
@@ -21,21 +39,18 @@ for entry in response.entries:
 
 def menu():
     while True:
-        print("1 - List feed")
+        print("\n1 - List feed")
         print("2 - Add feed")
-        print("3 - Remove feed")
-        print("x - Exit Application")
+        print("3 - Remove feeds")
+        print("x - Exit Application\n")
         op = input().split()
         if op:
             if op[0] == "1":
-                print("List feed")
-                break
+                listFeeds()
             elif op[0] == "2":
-                print("Add feed")
-                break
+                addFeed()
             elif op[0] == "3":
-                print("Remove feed")
-                break
+                removeFeeds()
             elif op[0] == "x":
                 break
 
