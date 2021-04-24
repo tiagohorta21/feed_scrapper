bca7bde9e5b973a1cc5d584fb6f8d9e318bd369d
Tiago Horta <tiago_horta95@hotmail.com>
2021-04-24 22:26:43 +0100

    main: Add feeds from text file

--- main.py
+++ main.py
@@ -7,22 +7,30 @@ import feedparser
 # Fox News - http://feeds.foxnews.com/foxnews/latest
 # USA Today - http://rssfeeds.usatoday.com/UsatodaycomNation-TopStories
 
+
+def getActiveState(active):
+  if(active):
+    return "Activated"
+  else:
+    return "Deactivated"
+
+
 def listFeeds():
     # Open database connection
     connection = sqlite3.connect("tp1.db")
     cursor = connection.cursor()
+
     # List all feeds from the database
     cursor.execute("SELECT * FROM Feeds")
     feeds = cursor.fetchall()
 
     print("\nCurrent feeds:")
     for count, feed in enumerate(feeds):
-      # List only the active feeds
-      if(feed[4]):
-        print(str(count + 1) + " - " + feed[1])
+      print(str(count + 1) + " - " + feed[1] + " | " + getActiveState(feed[4]) + " | URL: " + feed[2])
+
 
 def addFeed(feed):
-    # Get CNN feed
+    # Parse received feed
     response = feedparser.parse(feed)
     
     # Open database connection
@@ -42,6 +50,7 @@ def addFeed(feed):
     # We can also close the connection if we are done with it.
     # Just be sure any changes have been committed or they will be lost.
     connection.close()
+
 
 def removeFeed(feed):
     # Open database connection
@@ -73,11 +82,12 @@ def activateFeed(feed):
     # Just be sure any changes have been committed or they will be lost.
     connection.close()
 
+
 def deactivateFeed(feed):
     # Open database connection
     connection = sqlite3.connect("tp1.db")
     cursor = connection.cursor()
-    
+
     # Activate feed
     cursor.execute("UPDATE Feeds SET active='0' WHERE url=?", (feed,))
 
@@ -87,14 +97,25 @@ def deactivateFeed(feed):
     # Just be sure any changes have been committed or they will be lost.
     connection.close()
 
-def menu():
 
+def addFeedsFromTextFile():
+  # Open file and read each line
+  file = open('feeds.txt', 'r')
+  lines = file.readlines()
+
+  for line in lines:
+    print(f"Adding feed: {line}")
+    addFeed(line)
+
+
+def menu():
     while True:
         print("\n1 - List all feeds")
         print("2 - Add feed from url")
         print("3 - Remove feed from url")
         print("4 - Activate Feed")
         print("5 - Deactivate Feed")
+        print("6 - Add feeds from text file")
         print("x - Exit Application\n")
         op = input().split()
         if op:
@@ -116,9 +137,11 @@ def menu():
                 print("\nPaste the RSS feed you want to deactivate: ")
                 feed = input()
                 deactivateFeed(feed)
+            elif op[0] == "6":
+                print("\nAdd feeds from text file: ")
+                addFeedsFromTextFile()
             elif op[0] == "x":
                 break
-
 
 if __name__ == "__main__":
     menu()
